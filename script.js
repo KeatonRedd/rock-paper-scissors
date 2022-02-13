f3c8099fd2a48002076fc9b6f1abb9c6267019b0
KeatonRedd <keaton.redd@gmail.com>
2022-02-13 11:07:20 -0600

    finished

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+const buttonsArray = document.querySelectorAll('button');
+let playerWins = 0, computerWins = 0;
+
+buttonsArray.forEach((button) => {
+    button.addEventListener('click', () => {
+        buttonsArray.forEach((b) => {
+            b.classList.remove('clickedButton');
+        });
+
+        let computerChoice = computerPlay();
+        let gameResult = RPS(playerPlay(button.id), computerChoice);
+        button.classList.toggle('clickedButton');
+
+        if (gameResult == 'Win') {
+            playerWins++;
+        }
+
+        else if (gameResult == 'Loss') {
+            computerWins++;
+        }
+ 
+        document.querySelector('#computerSays').textContent = `Computer chose ${computerChoice}`;
+        document.querySelector('#currentGame').textContent = `Game resulted in a ${gameResult}`;
+        document.querySelector('#results').textContent = `Player Wins: ${playerWins} Computer Wins: ${computerWins}`;
+
+        if (playerWins == 5 || computerWins == 5) {
+            if (playerWins == 5) {
+                document.querySelector('#results').textContent = 'Player WINS THE GAME!!!';
+            }
+
+            else if (computerWins == 5) {
+                document.querySelector('#results').textContent = 'Computer WINS THE GAME!!!';
+            }
+
+            playerWins = 0, computerWins = 0;
+        }
+    });
+});
+
 function computerPlay() {
     let choice = Math.floor(Math.random() * 3);
 
@@ -27,10 +66,8 @@ function choiceTranslator(choice) {
     }
 }
 
-function playerPlay() {
-    let playerChoice = prompt('Type Rock, Paper, or Scissors').toLowerCase();
-
-    return choiceTranslator(playerChoice);
+function playerPlay(id) {
+    return choiceTranslator(id);
 }
 
 function RPS(playerSelection, computerSelection) {
@@ -38,7 +75,7 @@ function RPS(playerSelection, computerSelection) {
         if (computerSelection == 'Rock')
             return 'Draw';
         else if (computerSelection == 'Paper')
-            return 'Lose';
+            return 'Loss';
         else if (computerSelection == 'Scissors')
             return 'Win';
     }
@@ -49,12 +86,12 @@ function RPS(playerSelection, computerSelection) {
         else if (computerSelection == 'Paper')
             return 'Draw';
         else if (computerSelection == 'Scissors')
-            return 'Lose';
+            return 'Loss';
     }
 
     else if (playerSelection == 'Scissors') {
         if (computerSelection == 'Rock')
-            return 'Lose';
+            return 'Loss';
         else if (computerSelection == 'Paper')
             return 'Win';
         else if (computerSelection == 'Scissors')
@@ -62,7 +99,7 @@ function RPS(playerSelection, computerSelection) {
     }
 }
 
-function game() {
+/* function game() {
     let playerScore = 0;
     let computerScore = 0;
 
@@ -71,12 +108,12 @@ function game() {
             case 'Win':
                 playerScore++;
                 break;
-            
+
             case 'Lose':
                 computerScore++;
                 break;
 
-            default: 
+            default:
                 i--;
                 break;
         }
@@ -84,12 +121,10 @@ function game() {
         console.log(`Player Score: ${playerScore} Computer Score: ${computerScore}`)
     }
 
-    if (playerScore > computerScore){
+    if (playerScore > computerScore) {
         console.log('Player wins!');
     }
 
     else
         console.log('Player lost.');
-}
-
-game();
+}  */
